1dc2b239fec418329fcd32baede925cfae41e173
Łukasz Moszczyński <92304821+XanDuri@users.noreply.github.com>
2024-05-17 17:02:01 +0200

    Update settings.py

--- wizard/settings.py
+++ wizard/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k%84w15v#-4qhbs1c*qe+kyli%z#_a+hwib32#y__db8z7fuer'
+SECRET_KEY = 'SECRET'
 
 
 PAYNOW_API_KEY = 'YOUR_API_KEY'
